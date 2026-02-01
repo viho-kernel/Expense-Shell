20b68d4b20bb03f8d050428e352474f389ca5692
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-01 17:31:36 +0530

    latest commit

---
+++ frontend.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+P="\e[35m"
+C="\e[36m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+SCRIPT_DIR=$pwd 
+LOG_FOLDER="/var/log/Expense-Project-LOGS"
+LOG_FILE="$LOG_FOLDER/$0.log"
+MYSQL_HOST="mysql.opsora.space"
+password="ExpenseApp@1"
+
+mkdir -p $LOG_FOLDER
+
+if [ $USER_ID -ne 0 ]; then
+   echo -e " $R User is not Root. Kindly run the script as Root user. $N "
+   exit 1
+fi
+
+VALIDATE() {
+
+if [ $1 -ne 0 ]; then
+    echo -e " $R $2... Failed " | tee -a $LOG_FILE
+else
+    echo -e "$G $2... Success" | tee -a $LOG_FILE
+fi 
+
+}
+
+dnf install nginx -y  &>> $LOG_FILE
+
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>> $LOG_FILE
+
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>> $LOG_FILE
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
+VALIDATE $? "Removing Default Nginx server"
+
+curl -o /tmp/frontend.zip https://expense-joindevops.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>> $LOG_FILE
+VALIDATE $? "Installing cODE"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip &>> $LOG_FILE
+VALIDATE $? "Unzipping the code."
+
+
+cp $SCRIPT_DIR/frontend.conf /etc/nginx/default.d/expense.conf &>> $LOG_FILE
+VALIDATE $? "Copying the code."
+
+systemctl restart nginx &>> $LOG_FILE
+VALIDATE $? "Restarting Nginx Service"
+
